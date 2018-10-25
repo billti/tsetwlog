52a1c923c7e9b6cc1a4e7fa8a6b60895fb18f3e4
Bill Ticehurst <billti@hotmail.com>
2018-10-24 17:57:58 -0700

    Return undefined for the module if it didn't load

--- index.js
+++ index.js
@@ -2,12 +2,16 @@
 // There will be no exports if the module cannot be loaded
 
 // Only runs on Windows, on 32/64-bit Intel architectures
+var loaded = false;
 if (process && (process.arch === "ia32" || process.arch === "x64") && process.platform === "win32") {
     // Check Node.js requirements. The N-API usage in the module requires Node.js 10.2 or later
     if (process.versions && process.versions.node) {
         var nodeVersionParts = process.versions.node.split('.');
         if (parseInt(nodeVersionParts[0]) === 10 && parseInt(nodeVersionParts[1]) >= 2 || parseInt(nodeVersionParts[0]) > 10) {
             module.exports = require(`./${process.arch}/tsetwlog.node`);
+            loaded = true;
         }
     }
 };
+// Return no module whatsoever if the native module didn't get loaded
+if (!loaded) module.exports = undefined;
